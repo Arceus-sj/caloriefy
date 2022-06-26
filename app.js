2fcb343cbe0c166f2f22edb08ab17d11c06f6119
Shantanu Jana <omni.7x@gmail.com>
2022-06-26 19:44:36 +0530

    updated project

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-function MifflinsFornula(gender, age, bodyFat, height, weight) {
+function mifflinsFornula(gender, age, bodyFat, height, weight) {
+    // alert('working 1');
     let bmr = (10*weight) + (6.25*height) - (5*age) + 5;
     if(gender == 'Female')
     {
@@ -8,7 +9,8 @@ function MifflinsFornula(gender, age, bodyFat, height, weight) {
     return bmr;
 }
 
-function HarrisFornula(gender, age, bodyFat, height, weight) {
+function harrisFornula(gender, age, bodyFat, height, weight) {
+    // alert('working 2');
     let bmr = (13.397*weight) + (4.799*height) - (5.677*age) + 88.362;
     if(gender == 'Female') // Female
     {
@@ -19,6 +21,7 @@ function HarrisFornula(gender, age, bodyFat, height, weight) {
 }
 
 function katchsFornula(bodyFat, weight) {
+    // alert('working 3');
     let bmr = 370 + 21.6*(1 - (bodyFat/100))*weight;
 
     return bmr;
@@ -48,19 +51,26 @@ calculateBtn.addEventListener('click', function () {
     var formula = document.querySelector('#formula');
     var formulaOptionChose = formula.options[formula.selectedIndex].value;
     
-    let bmr = 0;
+    let bmr = '';
 
-    if (formulaOptionChose === 1) {
-        bmr = MifflinsFornula(gender, age, bodyFat, height, weight);
+
+    // console.log(formulaOptionChose);
+
+    if (formulaOptionChose == 1) {
+        bmr = mifflinsFornula(gender, age, bodyFat, height, weight);
+        
     }
-    else if(formulaOptionChose === 2) {
-        bmr = HarrisFornula(gender, age, bodyFat, height, weight);
+    else if(formulaOptionChose == 2) {
+        bmr = harrisFornula(gender, age, bodyFat, height, weight);
+        // alert('2');
     }
-    else if(formulaOptionChose === 3 ) {
+    else if(formulaOptionChose == 3 ) {
         bmr = katchsFornula(bodyFat, weight);
+        // alert('3');
+        // console.log(bmr);
     }    
 
-    console.log(bmr);
+    // console.log(bmr);
 
     let res = parseFloat(bmr) * parseFloat(activityOptionChose);
 
